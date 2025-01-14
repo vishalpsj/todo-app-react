09d3b0d7b4e0044b2417e1450476dd7165a33caa
Vishal.P <73173958+vishalpsj@users.noreply.github.com>
2025-01-14 23:10:21 +0530

    tasks will now show as we enter it

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './Todo.css'
+import { MdCheck, MdDeleteForever } from "react-icons/md";
+
 
 export const Todo = () => {
 
@@ -9,14 +11,14 @@ export const Todo = () => {
     const handleFormSumbmit = (e) => {
         e.preventDefault()
 
-        
-        if(!inputValue)return
-        
-        if(task.includes(inputValue)){
+
+        if (!inputValue) return
+
+        if (task.includes(inputValue)) {
             setinputValue("")
             return
         }
-        
+
         setTask((prevTask) => [...prevTask, inputValue])
         setinputValue("")
 
@@ -27,9 +29,29 @@ export const Todo = () => {
                 <h1>To do App</h1>
                 {/* <div className="time"></div> */}
                 <form onSubmit={handleFormSumbmit} className="input">
-                    <input type="text" placeholder="Enter the task here..."autoComplete="off" value={inputValue} onChange={(e) => setinputValue(e.target.value)} />
+                    <input type="text" placeholder="Enter the task here..." autoComplete="off" value={inputValue} onChange={(e) => setinputValue(e.target.value)} />
                     <button type="submit">Add</button>
                 </form>
+
+                <div className="todoSection">
+                    <ul className='unorderedList'>
+                        {
+                            task.map((curTask, index) => {
+                                return (
+                                    <li key={index} className='todoItem'>
+                                        <div className="todoTask">
+                                            {curTask}
+                                        </div>
+                                        <div className="todoControl">
+                                            <MdCheck className='check'/>
+                                            <MdDeleteForever className='dlt' />
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </>
     )
