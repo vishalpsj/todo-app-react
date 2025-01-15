ae4fdbb1dfb1cc2b25b3c48bb61c3b431b015665
Vishal.P <73173958+vishalpsj@users.noreply.github.com>
2025-01-15 15:43:22 +0530

    week added with date, css modified

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -2,28 +2,45 @@
 
 import { useEffect, useState } from 'react'
 import './DateTime.css'
+import '../responsive.css'
 
 export const DateTime = () => {
 
-    const date = new Date().toLocaleDateString()
+    const dateFormat = {
+        weekday: "long", // Shows the full name of the week (e.g., Monday)
+        year: "numeric",
+        month: "long",  // Full month name (e.g., January)
+        day: "numeric"  // Day of the month
+    }
 
+    // const date = new Date().toLocaleDateString(undefined, dateFormat)
+
+    const [date, setdate] = useState(new Date().toLocaleDateString(undefined, dateFormat))
     const [time, settime] = useState(new Date().toLocaleTimeString())
 
 
     useEffect(() => {
         const intervalId = setInterval(() => {
+            setdate(new Date().toLocaleDateString(undefined, dateFormat))
             settime(new Date().toLocaleTimeString())
         }, 1000);
         return () => clearInterval(intervalId)
 
-    },[])
+    }, [])
 
 
 
 
     return (
         <>
-            <p className="dateTime">{date} - {time}</p>
+            <div className ="dateTime">
+                <div className="dateis">
+                    {date}
+                </div>
+                <div className="timeis">
+                    {time}
+                </div>
+            </div>
         </>
     )
 }
